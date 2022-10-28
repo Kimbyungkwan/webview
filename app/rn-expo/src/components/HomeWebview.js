16aef3235d47ffc655f5dfd6c66d9f4fd5f50e22
Kimbyungkwan <kqudk@naver.com>
2022-10-29 01:03:08 +0900

    feat: rn webview 뒤로가기

--- app/rn-expo/src/components/HomeWebview.js
+++ app/rn-expo/src/components/HomeWebview.js
@@ -1,12 +1,31 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { useCallback, useEffect, useRef } from 'react';
+import { BackHandler, SafeAreaView, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function HomeScreen() {
+  const webviewRef = useRef();
+
+  const goback = useCallback(() => {
+    if (!webviewRef.current) return false;
+    webviewRef.current.goBack();
+    return true;
+  }, [webviewRef.current]);
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', goback);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', goback);
+    };
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{ uri: 'http://127.0.0.1:3030' }} />
-      {/* expo go에서 테스트하려면 본인 ip 넣어줌 */}
-      {/* <WebView source={{ uri: 'http://127.0.0.1:3030' }} /> */}
+      <WebView
+        allowsBackForwardNavigationGestures
+        ref={webviewRef}
+        /* expo go에서 테스트하려면 본인 ip 넣어줌 */
+        source={{ uri: 'http://127.0.0.1:3030' }}
+      />
     </SafeAreaView>
   );
 }
